72a6f11ba425d5b0a0ff77da5b6591fc7a74abdb
yonghyoekchoi <99475405+yonghyoekchoi@users.noreply.github.com>
2025-03-13 23:38:01 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://oldhwuqmpnklmgujpfuc.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZGh3dXFtcG5rbG1ndWpwZnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NDE1MDQsImV4cCI6MjA1NTAxNzUwNH0.5xhmfrOhmWcJYRQSi5ld8mVKCjRp5bDJlTKJb5MGxl8',
+        '',
   );
   runApp(const MyApp());
 }
